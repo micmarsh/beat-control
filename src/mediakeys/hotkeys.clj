f53238d9a559d1b6ad909784096ca583f7230dbc
micmarsh <michael.marsh42@gmail.com>
2014-03-28 20:03:00 -0400

    some functions that can be used to structure core.async flow

--- src/mediakeys/hotkeys.clj
+++ src/mediakeys/hotkeys.clj
@@ -1,6 +1,6 @@
 (ns mediakeys.hotkeys
     (:use [mediakeys.file :only [DEFAULT_KEYS save-keys!]]
-           mediakeys.rx))
+           clojure.core.async))
 
 (import [com.tulskiy.keymaster.common Provider HotKeyListener])
 (import [javax.swing KeyStroke])
@@ -40,3 +40,24 @@
                       (.stop))
                   (reset! provider new-provider)
                   result)))))
+
+(def new-provider
+    (let [provider (atom nil)]
+        (fn [_]
+            (when @provider
+                (doto @provider
+                    (.reset)
+                    (.stop)))
+            (reset! provider 
+                (Provider/getCurrentProvider false))
+            @provider)))
+
+(defn register-keys [^Provider provider keys]
+    (let [return (chan)]
+        (doseq [[action hotkey] item
+                 keystroke [(make-keystroke hotkey)]]
+                (.register provider keystroke
+                    (proxy [HotKeyListener] []
+                        (onHotKey [event]
+                            (put! return action)))))
+        return))
