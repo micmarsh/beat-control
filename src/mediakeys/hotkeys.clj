e56cbeab0823bc3e66d09fb0dd1551699a0c47c2
micmarsh <michael.marsh42@gmail.com>
2014-03-31 10:17:33 -0400

    tentative "functional" structure using core.async

--- src/mediakeys/hotkeys.clj
+++ src/mediakeys/hotkeys.clj
@@ -5,75 +5,6 @@
 (import [com.tulskiy.keymaster.common Provider HotKeyListener])
 (import [javax.swing KeyStroke])
 
-(defn make-keystroke [keystroke]
-    (KeyStroke/getKeyStroke keystroke))
-
-(def current-keys (atom DEFAULT_KEYS))
-
-(defn register-keys! [^Provider provider keys]
-    (observable 
-        (fn [s]
-            (doseq [[action hotkey] keys
-                    keystroke [(make-keystroke hotkey)]]
-                (.register provider keystroke
-                    (proxy [HotKeyListener] []
-                        (onHotKey [event]
-                            (next! s action))))))))
-
-(def provider (atom (Provider/getCurrentProvider false)))
-
-(defn key-config! [key-changes]
-    (map key-changes
-        (fn [key-update]
-            (swap! current-keys #(merge % key-update))
-            (save-keys! @current-keys)
-            @current-keys)))
-
-(defn keypress-events! [key-changes]
-    (-> key-changes
-        key-config!
-        (mapcat (fn [all-keys]
-            (let [new-provider (Provider/getCurrentProvider false)
-                  result (register-keys! new-provider all-keys)]
-                  (doto @provider
-                      (.reset)
-                      (.stop))
-                  (reset! provider new-provider)
-                  result)))))
-
-(def new-provider!
-    (let [provider (atom nil)]
-        (fn [_]
-            (when @provider
-                (doto @provider
-                    (.reset)
-                    (.stop)))
-            (reset! provider 
-                (Provider/getCurrentProvider false))
-            @provider)))
-
-(defn register-keys [^Provider provider keys]
-    (let [return (chan)]
-        (doseq [[action hotkey] keys
-                 keystroke [(make-keystroke hotkey)]]
-                (.register provider keystroke
-                    (proxy [HotKeyListener] []
-                        (onHotKey [event]
-                            (put! return action)))))
-        return))
-
-; (defn mapcat< [f channel]
-;     (let [return (chan)]
-;         (go-loop []
-;             (let [value channel]
-;                 (if value)))))
-
-;  mapcat-last<
-;   somehow use an atom bound when return is made and reset it as part of the 
-;   "macro" loop somehow
-;       
-;
-
 (def not-nil? (comp not nil?))
 
 (defn flatmap< [f channel]
@@ -89,14 +20,49 @@
                     (recur))))
         return))
 
-(def numbers (chan))
+(def new-provider!
+    (let [provider (atom nil)]
+        (fn [ ]
+            (when @provider
+                (doto @provider
+                    (.reset)
+                    (.stop)))
+            (reset! provider 
+                (Provider/getCurrentProvider false))
+            @provider)))
 
-(doseq [i (range 10)]
-    (put! numbers i))
+(def new-keys!
+    (let [keys (atom nil)]
+        (fn [key-update]
+            (swap! keys #(merge key-update))
+            @keys)))
 
-(defn silly-double [number]
-    (let [c (chan)]
-        (put! c (* 2 number))
-        c))
+(defn register-keys [^Provider provider keys]
+    (let [return (chan)]
+        (doseq [[action hotkey] keys
+                 keystroke [(make-keystroke hotkey)]]
+                (.register provider keystroke
+                    (proxy [HotKeyListener] []
+                        (onHotKey [event]
+                            (put! return action)))))
+        return))
 
-(def dubs (flatmap< silly-double numbers))
+(defn keypress-channel! [key-changes]
+    (let [old-keys (new-keys! { })]
+        (try
+            (let [keys (new-keys! key-changes)
+                  provider (new-provider!)
+                  channel (register-keys provider keys)]
+                (save-keys! keys)
+                channel)
+        (catch java.lang.Exception e
+            (println "here's the exception, catch it more specifically")
+            (.printStackTrace e)
+            (new-keys! old-keys)
+            (register-keys (new-provider!) old-keys)
+            ; TODO grab that^ channel before returning it, 
+            ; and pass some error info into it
+            ))))
+
+(def keypress-events! (partial flatmap< keypress-channel!))
+
