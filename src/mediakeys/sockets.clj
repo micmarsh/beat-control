a0a6aed340f4e2d5e3c414e8216720a81eb5dcdc
micmarsh <michael.marsh42@gmail.com>
2014-04-13 15:45:15 -0400

    trying some garbage collection of old channels

--- src/mediakeys/sockets.clj
+++ src/mediakeys/sockets.clj
@@ -1,6 +1,6 @@
 (ns mediakeys.sockets
     (:use [mediakeys.utils :only [defcurried dochan]]
-          [clojure.core.async :only [put! chan tap]])
+          [clojure.core.async :only [put! chan close! tap]])
     (:import [org.webbitserver WebSocketHandler]))
 
 (defcurried send! [c message] 
@@ -11,24 +11,44 @@
         (tap mult return)
         return))
 
+(def channels (atom { }))
+(defn start! [conn channel]
+    (swap! channels assoc conn channel)
+    (print @channels))
+(defn stop! [conn]
+    (close! (@channels conn))
+    (swap! channels dissoc conn)
+    (print @channels))
+
 (defn keypresses [incoming]
     (proxy [WebSocketHandler] []
         (onOpen [c]
-            (dochan (clone incoming) (send! c)))
-        (onClose [c] (println "closed" c))
+            (let [channel (clone incoming)]
+                (dochan channel (send! c))
+                (start! c channel)))
+        (onClose [c] 
+            (println "closed" c)
+            (stop! c))
         (onMessage [c j] )))
 
 (defn controls [new-keys incoming-changes]
     (proxy [WebSocketHandler] []
         (onOpen [c]
-            (dochan (clone incoming-changes) (send! c)))
-        (onClose [c] (println "closed changes" c))
+            (let [channel (clone incoming-changes)]
+                (dochan channel (send! c))
+                (start! c channel)))
+        (onClose [c] 
+            (println "closed changes" c)
+            (stop! c))
         (onMessage [c message] 
             (put! new-keys message))))
 
 (defn errors [incoming-errors]
     (proxy [WebSocketHandler] []
         (onOpen [c]
-            (dochan (clone incoming-errors) (send! c)))
-        (onClose [c])
+           (let [channel (clone incoming-errors)]
+                (dochan channel (send! c))
+                (start! c channel)))
+        (onClose [c]
+            (stop! c))
         (onMessage[c j])))
