54fe80e0673f54fe84d7bc409f60bedd336504f5
micmarsh <michael.marsh42@gmail.com>
2014-04-12 22:42:24 -0400

    can clone channels!

--- src/mediakeys/sockets.clj
+++ src/mediakeys/sockets.clj
@@ -1,22 +1,27 @@
 (ns mediakeys.sockets
     (:use [mediakeys.utils :only [defcurried dochan]]
-          [clojure.core.async :only [put!]])
+          [clojure.core.async :only [put! chan mult tap]])
     (:import [org.webbitserver WebSocketHandler]))
 
 (defcurried send! [c message] 
     (.send c message))
 
+(defn clone [channel]
+    (let [return (chan)]
+        (tap (mult channel) return)
+        return))
+
 (defn keypresses [incoming]
     (proxy [WebSocketHandler] []
         (onOpen [c]
-            (dochan incoming (send! c)))
+            (dochan (clone incoming) (send! c)))
         (onClose [c] (println "closed" c))
         (onMessage [c j] )))
 
 (defn controls [new-keys incoming-changes]
     (proxy [WebSocketHandler] []
         (onOpen [c]
-            (dochan incoming-changes (send! c)))
+            (dochan (clone incoming-changes) (send! c)))
         (onClose [c] (println "closed changes" c))
         (onMessage [c message] 
             (put! new-keys message))))
@@ -24,6 +29,6 @@
 (defn errors [incoming-errors]
     (proxy [WebSocketHandler] []
         (onOpen [c]
-            (dochan incoming-errors (send! c)))
+            (dochan (clone incoming-errors) (send! c)))
         (onClose [c])
         (onMessage[c j])))
